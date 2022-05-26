45149d19c09a012733eecf25f006c5e965e02aee
Elnur K. Mehdizada <elnurkm@code.local>
2022-05-26 10:42:22 -0700

    I added my first visual studio code to github

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sales = 456;
+            int price = 4;
+
+            int total = sales * price;
+
+
+            string salesmanager = "ceyhun";
+            bool issalesquite = true;
+
+
+
+            Console.WriteLine(total+" "+salesmanager+" "+issalesquite) ;
+            
+
+        }
+    }
+}
